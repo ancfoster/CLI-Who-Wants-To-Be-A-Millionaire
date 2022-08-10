386b41fa57805a4d2bac3779d487d9f02d4b5590
ancfoster <a.foster@outlook.com>
2022-08-10 12:19:39 +0100

    Fix bug when removing question ID from question array

--- run.py
+++ run.py
@@ -79,27 +79,29 @@ def new_game():
     clear_output(1)
     global question_number
     question_number = 1
-    global question_numbers_array
-    question_numbers_array = []
     load_level(1)
 
-def load_level(q_number):
+def load_level(level_number):
     """
     This function loads the 'level' variables.
     There are three sheets in the worksheet file, one for easy questions, medium and hard.
     The difficulty of question picked depends on the players progress.
     """
     global sheet
-    if q_number < 6:
+    if level_number == 1:
         sheet = 'easy_questions'
-    elif q_number < 11:
+    elif level_number == 6:
         sheet = 'medium_questions'
-    elif q_number < 16:
+    elif level_number == 11:
         sheet = 'hard_questions'
-    # Loads the required sheet and counts the number of questions.
+    print(sheet)
+    # Loads the required sheet and counts the number of questions in the sheet.
+    global loaded_sheet
     loaded_sheet = SHEET.worksheet(sheet)
     column1 = loaded_sheet.col_values(1)
     number_of_questions_in_sheet = len(column1)
+    global question_numbers_array
+    question_numbers_array = []
     question_array_counter = 1
     # Creates an array of numbers based on the number of questions in the sheet.
     while question_array_counter < number_of_questions_in_sheet:
@@ -114,51 +116,53 @@ def load_question():
     A row contains a written question and four answers. The last answer is always the correct one.
     """
     print("Loading question . . .")
+    #question ID represents the list index of each question number
     global question_numbers_array
     question_id = random.randrange(1, len(question_numbers_array))
-    #remove question ID from question_numbers_array
-    question_numbers_array.remove(question_id)
+    print(f"ID: {question_id} \n Array: {question_numbers_array}")
     loaded_sheet = SHEET.worksheet(sheet)
-    #Add one because first row contains column headers
     global question_row
-    question_row = loaded_sheet.row_values(question_id + 1)
-    clear_output(0.5)
+    question_row = loaded_sheet.row_values(question_numbers_array[question_id])
+    question_numbers_array.pop(question_id)
+    print(question_numbers_array)
     ask_question()
+    clear_output(2)
     
 def display_question_amount(q_number):
-        match q_number:
-            case 1:
-                return '£100'
-            case 2:
-                return '£200'
-            case 3:
-                return '£300'
-            case 4:
-                return '£500'
-            case 5:
-                return '£1,000'
-            case 6:
-                return '£2,000'
-            case 7:
-                return '£5,000'
-            case 8:
-                return '£10,000'
-            case 9:
-                return '£20,000'
-            case 10:
-                return '£50,000'
-            case 11:
-                return '£75,000'
-            case 12:
-                return '£150,000'
-            case 13:
-                return '£250,000'
-            case 14:
-                return '£500,000'
-            case 15:
+    match q_number:
+        case 1:
+            return '£100'
+        case 2:
+            return '£200'
+        case 3:
+            return '£300'
+        case 4:
+            return '£500'
+        case 5:
+            return '£1,000'
+        case 6:
+            return '£2,000'
+        case 7:
+            return '£5,000'
+        case 8:
+            return '£10,000'
+        case 9:
+            return '£20,000'
+        case 10:
+            return '£50,000'
+        case 11:
+            return '£75,000'
+        case 12:
+            return '£150,000'
+        case 13:
+            return '£250,000'
+        case 14:
+            return '£500,000'
+        case 15:
                 return '£1,000,000'
 
 def ask_question():
+    print(sheet)
     question_money = display_question_amount(question_number)
     print(f"Question {question_number} for {question_money}:\n")
     print(f"{question_row[0]}\n")
@@ -204,7 +208,9 @@ def level_check():
     """
     global question_number
     question_number += 1
-    
+
+    print(f"Level check question number = {question_number}")
+
     if question_number == 6:
         load_level(2)
     elif question_number == 11:
