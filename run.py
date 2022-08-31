6c82aa020bab549e8437888f8240e333dbb70d1a
ancfoster <a.foster@outlook.com>
2022-08-31 11:45:21 +0100

    Fix how to play section to display properly on Heroku terminal

--- run.py
+++ run.py
@@ -349,26 +349,28 @@ def how_to():
     clear_output(0)
     print(f"How to play CLI Who Wants to Be A Millionaire\n")
     print(f"_____________________________________________\n")
-    print(f'''This game is a CLI interpretation of the UK quiz\n
-    game show of the same name. 
-    \n
-    The objective is to win £1,000,000 by answer 15 multiple choice questions\n
-    correctly. These questions are from a variety of general knowledge topics.\n
-    The questions will get harder as the game progresses. \n
-    The questions are valued at progressively higher sums of money. \n
-    The questions answers are labelled A, B, C and D. To answer a question\n
-    simply input the corresponding letter into the terminal and press the enter key.\n
-    During the game there are two monetary milestones, £1,000 and £50,000.\n
-    If you answer a question incorrectly, but passed a milestone during the game\n
-    that milestone will be your score. 
-    \n
-    Game developed by Alexander Foster.
-    \n\n       
-    ''')
+    print(f'''This game is a CLI interpretation of the UK quiz
+game show of the same name. 
+
+The objective is to win £1,000,000 by answer 15 multiple choice questions
+correctly. These questions are from a variety of general knowledge topics.
+The questions will get harder as the game progresses.
+The questions are valued at progressively higher sums of money.\n''')
+    input("Press Enter to continue reading . . .")
+    clear_output(0)
+    print('''
+The questions answers are labelled A, B, C and D. To answer a question
+simply input the corresponding letter into the terminal and press the
+enter key. During the game there are two monetary milestones, £1,000
+and £50,000. If you answer a question incorrectly, but passed a 
+milestone during the game that milestone will be your score. 
+
+Game developed by Alexander Foster.
+''')
     while True:
         print('''Navigate back Main Menu by entering "menu"
 or start a new game by entering "new"''')
-        how_to_input = input("\nEnter input: \n")
+        how_to_input = input("\nEnter input: ")
         how_to_input = how_to_input.lower()
         if validate_how_to_play(how_to_input):
             break
@@ -388,7 +390,8 @@ def validate_how_to_play(input):
     elif input == 'menu':
         return True
     else:
-        print(f"Ivalid input, you entered {input}")
+        clear_output(0)
+        print(f"Ivalid input, you entered {input}\n")
         return False
 
 
